a794bc07811a2b83863a949c0ee29013576f0d58
Krista <37194474+KristaMarieB@users.noreply.github.com>
2018-10-15 13:41:39 -0700

    Added documention for the class and first method

--- HometownZoo.Core/Class1.cs
+++ HometownZoo.Core/Class1.cs
@@ -3,8 +3,16 @@ using System.Text.RegularExpressions;
 
 namespace HometownZoo.Core
 {
+    /// <summary>
+    /// Contains generic validation helper methods
+    /// </summary>
     public static class Validator
     {
+        /// <summary>
+        /// Validates a given zip code for a US format
+        /// </summary>
+        /// <param name="zip">The input to check</param>
+        /// <returns></returns>
         public static bool IsValidUSZipCode(string zip)
         {
             Regex pattern = new Regex(@"^\d{5}-?\d{4}?$");
