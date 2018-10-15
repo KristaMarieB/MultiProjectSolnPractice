cd58f25794f98ef370988431f82e7cf7aa006e83
Krista <37194474+KristaMarieB@users.noreply.github.com>
2018-10-15 14:17:34 -0700

    Created another class in the business logic project (HometownZoo.Core). Added properties and documention.

---
+++ HometownZoo.Core/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HometownZoo.Core
+{
+    /// <summary>
+    /// Represents a any person (i.e. zoo member, employee, etc.)
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Primary key field
+        /// </summary>
+        public int PersonID { get; set; }
+
+        /// <summary>
+        /// The person's first AND last name
+        /// </summary>
+        public string Name { get; set; }
+        
+    }
+}
