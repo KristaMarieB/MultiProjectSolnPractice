772c151f190e57ad1a22b980bde88ad1b62095aa
Krista <37194474+KristaMarieB@users.noreply.github.com>
2018-10-15 13:37:20 -0700

    Created a zipcode validator method using regular expression

--- HometownZoo.Core/Class1.cs
+++ HometownZoo.Core/Class1.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace HometownZoo.Core
 {
     public static class Validator
     {
+        public static bool IsValidUSZipCode(string zip)
+        {
+            Regex pattern = new Regex(@"^\d{5}-?\d{4}?$");
+            //regex could throw a lot of errors so we use the @ to say we want the literal string
+            //-? ? means optional 
 
+            return pattern.IsMatch(zip);
+        }
     }
 }
