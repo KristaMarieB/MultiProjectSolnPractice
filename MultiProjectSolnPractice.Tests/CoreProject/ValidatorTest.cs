5492a9f7de3e75fa39febd8b6660d231c30e74f9
Krista <37194474+KristaMarieB@users.noreply.github.com>
2018-10-15 14:45:20 -0700

    Added usings to HomeController. Created first unit test for the zipcode validator

---
+++ MultiProjectSolnPractice.Tests/CoreProject/ValidatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HometownZoo.Core.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiProjectSolnPractice.Tests.CoreProject
+{
+    [TestClass]
+    public class ValidatorTest
+    {
+        [TestMethod]
+        public void IsValidZipCode_USFormat_ReturnsTrue()
+        {
+
+            // Arrange
+            string input = "98499";
+
+            // Act
+            bool result = Validator.IsValidUSZipCode(input);
+
+            // Assert
+            Assert.IsTrue(result);
+            // we could do this: Assert.IsTrue(Validator.IsValidUSZipcode(input));
+            // but we'll stick with our structure for now to stay organized
+        }
+    }
+}
--- MultiProjectSolnPractice/Controllers/HomeController.cs
+++ MultiProjectSolnPractice/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HometownZoo.Core.Models;
+using HometownZoo.Core.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,20 @@ namespace MultiProjectSolnPractice.Controllers
     {
         public ActionResult Index()
         {
+            // did this so we could see another way of adding a reference
+            // right click references in this project, add-reference, check mark the one we want to add, click OK
+            // then add using to the file
+            Person p = new Person()
+            {
+                Name = "Krista Brassesco"
+            };
+
+            string randomZip = "98373";
+            if (Validator.IsValidUSZipCode(randomZip))
+            {
+                
+            }
+
             return View();
         }
 
